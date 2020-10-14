eec2611279b593929021fab6e736071cb0b77e15
HarrisonKawagoe3960X <hyzitbios@hotmail.com>
2020-10-14 21:00:05 +0900

    Initial commit

---
+++ app/src/main/java/com/hrsnkwge/myapplication/Browser.kt
@@ -0,0 +1,11 @@
+package com.hrsnkwge.myapplication
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class Browser : AppCompatActivity(){
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.browser)
+    }
+}
